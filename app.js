6e6ed03a73fd83c848158401d3b5277e341410f8
Lucas Simão <siimaoo.lucas@gmail.com>
2019-05-07 08:27:01 -0300

    Configurando conexão com a DB

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const produtoRoute = require('./routes/produtos');
+
+const url = 'url';
+const option = {reconnectTries: Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser: true};
+
+mongoose.connect(url, option);
+mongoose.set('useCreateIndex', true);
+
+mongoose.connection.on('error', (err) => {
+    console.log("Erro na conexao com o banco de dados! " + err);
+});
+mongoose.connection.on('disconnected', (err) => {
+    console.log("Desconectado do banco de dados! " + err);
+});
+mongoose.connection.on('connected', () => {
+    console.log('Conectado ao banco de dados!');
+});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use('/', produtoRoute);
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000!');
