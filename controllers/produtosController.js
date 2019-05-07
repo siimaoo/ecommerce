69653dca8fcc445d83d158501fd702bdda2a7770
Lucas Simão <siimaoo.lucas@gmail.com>
2019-05-07 08:25:27 -0300

    Atualização dos controllers

--- controllers/produtosController.js
+++ controllers/produtosController.js
@@ -10,14 +10,14 @@ module.exports = {
     },
     buscarId(req, res) {
         const id = req.params.id;
-        Produtos.findById({__id: id}, (err, data) => {
+        Produtos.findById({ _id: id }, (err, data) => {
             if (err) return res.status(400).send({ mensagem: "Erro ao realizar consulta!" });
             return res.status(200).send(data);
         });
     },
     adicionarProduto(req, res) {
-        const { nome, categoria, descricao, imagem1, imagem2, imagem3, thumbnail, valor, oferta, valorOferta, acessos, quantidade} = req.body;
-        if (!nome || !categoria || !descircao || !imagem1 || !thumbnail || !valor || !quantidade) return res.status(400).send({mensagem: "Preencha todos os campos!"});
+        const { nome, categoria, descricao, imagem1, imagem2, imagem3, thumbnail, valor, oferta, valorOferta, acessos, estoque } = req.body;
+        if (!nome || !categoria || !descricao || !imagem1 || !thumbnail || !valor || !estoque) return res.status(400).send({ mensagem: "Preencha todos os campos!" });
         Produtos.create(req.body, (err, data) => {
             if (err) return res.status(400).send({ mensagem: "Erro ao criar novo produto!" });
             return res.status(200).send(data);
@@ -25,16 +25,19 @@ module.exports = {
     },
     alterarProduto(req, res) {
         const id = req.params.id;
-        Produtos.findByIdAndUpdate(id, { $set:req.body }, (err, data) => {
+        Produtos.findByIdAndUpdate(id, { $set: req.body }, (err, data) => {
             if (err) return res.status(400).send({ mensagem: "Erro ao realizar atualização!" });
             return res.status(200).send(data);
         });
     },
     deletarProduto(req, res) {
         const id = req.params.id;
-        Produtos.delete({__id: id}, (err, data) => {
+
+        Produtos.deleteMany({ _id: id }, (err, data) => {
             if (err) return res.status(400).send({ mensagem: "Erro ao remover produto!" });
             return res.status(200).send(data);
         });
-    }
+
+    },
+  
 }
