05dc6d1aa93eb85105e2868c1c4c7ae842188216
Lucas Simão <siimaoo.lucas@gmail.com>
2019-06-20 17:08:14 -0300

    Adição de rota para validação de token

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const auth = require('../middlewares/auth');
+
 
 router.get('/', userController.buscar);
 router.post('/create', userController.criar);
 router.post('/auth', userController.auth);
+router.get('/validar-token', auth, userController.validacao);
 
 module.exports = router;
