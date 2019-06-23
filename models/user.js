93e26012bb946635c4793fac00319366e050c59f
Lucas Simão <siimaoo.lucas@gmail.com>
2019-06-23 18:52:58 -0300

    Alteração no schema dos usuarios

--- models/user.js
+++ models/user.js
@@ -5,6 +5,16 @@ const bcrypt = require('bcryptjs');
 const UserSchema = new Schema({
     email: { type: String, required: true, unique: true, lowercase: true },
     senha: {type: String, required: true, select: false},
+    nome: {type: String, required: true, select: true},
+    sobrenome: {type: String, required: true, select: true},
+    cpf: {type: String, required: true, select: true, unique: true,},
+    endereco: [{
+        logradouro: {type: String, required: true, select: true},
+        bairro: {type: String, required: true, select: true},
+        cidade: {type: String, required: true, select: true},
+        cep: {type: String, required: true, select: true},
+        estado: {type: String, required: true, select: true},
+    }],
     created: {type: Date, default: Date.now}
 });
 
